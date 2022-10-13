6ff1500912bae491e2b40840befc80018944c11f
nellizi <masterliz9095@gmail.com>
2022-10-13 14:46:09 +0900

    반환 객체타입을 object로 변경

--- src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
+++ src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
@@ -8,14 +8,26 @@ import java.util.Properties;
 class Car{}
 class SportsCar extends Car{}
 class Truck extends Car{}
+class Engine{}
+
 
 
 public class Main1 {
     public static void main(String[] args) throws Exception {
-        Car car = getCar();
+        Car car = (Car) getObject("car");
         System.out.println("car = " + car);
+        Engine engine = (Engine) getObject("engine");
+        System.out.println("engine = " + engine);
     }
 
+    static Object getObject(String key) throws Exception{
+        Properties p = new Properties();
+        p.load(new FileReader("config.txt"));
+
+        Class clazz = Class.forName(p.getProperty(key));
+
+        return clazz.newInstance();
+    }
     static Car getCar() throws Exception{
         Properties p = new Properties();
         p.load(new FileReader("config.txt"));
